17c0a85398ca2e47b21961d754c9bb2b90a02925
Swetha S <73575054+swetha-02@users.noreply.github.com>
2024-03-21 18:24:10 +0530

    Add files via upload

---
+++ My-Work/Day-4/hotel.py
@@ -0,0 +1,35 @@
+bf={
+    1:"Idly",
+    2:"Dosa",
+    3:"Pongal",
+    }
+break1={
+    1:"10",
+    2:"30",
+    3:"40"
+}
+
+lunch={
+    1:"Sambhar Rice",
+    2:"Lemon Rice",
+    3:"Variety Rice",  
+}
+lunch1={
+    1:"50",
+    2:"70",
+    3:"80"
+}
+
+
+dinner={
+    1:"Chapathi",
+    2:"Masal Dosa",
+    3:"Barotta",
+}
+dinner1={
+    1:"35",
+    2:"45",
+    3:"25"
+}
+
+hotel_name="A2B Hotel"
